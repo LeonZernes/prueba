9dd60b57e6541e11e413d5a4175d5a3dd10a3f50
LeonZernes <46255372+LeonZernes@users.noreply.github.com>
2019-07-04 11:10:43 +0100

    python 3 script install

---
+++ install_virtualENV.sh
@@ -0,0 +1,30 @@
+echo "Python 3 installer: installing necessary libraries"
+sudo apt-get update
+sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
+
+echo "Compiling (takes a while!"
+wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
+tar xf Python-3.7.0.tar.xz
+cd Python-3.7.0
+./configure --prefix=/usr/local/opt/python-3.7.0
+make -j 4
+
+echo "Installing new version of python"
+sudo make altinstall
+
+echo "Make Python 3.7 the default version, make aliases"
+sudo ln -s /usr/local/opt/python-3.7.0/bin/pydoc3.7 /usr/bin/pydoc3.7
+sudo ln -s /usr/local/opt/python-3.7.0/bin/python3.7 /usr/bin/python3.7
+sudo ln -s /usr/local/opt/python-3.7.0/bin/python3.7m /usr/bin/python3.7m
+sudo ln -s /usr/local/opt/python-3.7.0/bin/pyvenv-3.7 /usr/bin/pyvenv-3.7
+sudo ln -s /usr/local/opt/python-3.7.0/bin/pip3.7 /usr/bin/pip3.7
+alias python='/usr/bin/python3.7'
+alias python3='/usr/bin/python3.7'
+ls /usr/bin/python*
+cd ..
+sudo rm -r Python-3.7.0
+rm Python-3.7.0.tar.xz
+. ~/.bashrc
+
+echo "Verifying Python version"
+python -V
